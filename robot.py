6a232617e39336c48ecfe09f00838e2941427eaa
Jack Reinhardt <reinh240@umn.edu>
2019-03-12 22:24:30 +0100

    added move function

--- robot.py
+++ robot.py
@@ -1,8 +1,44 @@
-class Robot(object): 
+# Robot.
+#
+# This is the main class for the game pieces. This is where the pieces can be
+# moved. 
 
-    def __init__(self, Colour, current_x, current_y, current_square_x, current_square_y): 
-        self.colour = Colour 
-        self.curX = current_x 
-        self.curY = current_y 
-        self.curSx = current_square_x
-        self.curSy = current_square_y
+class Robot: 
+  def __init__(self, Colour, current_x, current_y, current_square_x, current_square_y): 
+    self.colour = Colour 
+    self.curX = current_x
+    self.curY = current_y 
+    self.curSx = current_square_x
+    self.curSy = current_square_y
+
+  # updates the position variables of the robot, updates board[][].robot
+  def move(self, board, d, l):
+    # test_robot = self
+    moved = False
+    if d == "NORTH":
+      while board[self.curSy][self.curSx].north == 0 and board[self.curSy-1][self.curSx].occ == 0:
+        self.curSy -= 1
+        self.curY -= l
+        moved = True
+        # update board[][].robot
+    elif d == "SOUTH":
+      while board[self.curSy][self.curSx].south == 0 and board[self.curSy+1][self.curSx].occ == 0:
+        self.curSy += 1
+        self.curY += l
+        moved = True
+        # update board[][].robot
+    elif d == "EAST":
+      while board[self.curSy][self.curSx].east == 0 and board[self.curSy][self.curSx+1].occ == 0:
+        self.curSx += 1
+        self.curX += l
+        moved = True
+        # update board[][].robot
+    elif d == "WEST":
+      while board[self.curSy][self.curSx].west == 0 and board[self.curSy][self.curSx-1].occ == 0:
+        self.curSx -= 1
+        self.curX -= l
+        moved = True
+        # update board[][].robot
+    else:
+      print("The key you entered is not a valid direction.")
+    return moved # test_robot.curX != self.curX or test_robot.curY != self.curY
