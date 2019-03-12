ecb5ceb01a1f4d3d0a9461a11152261694e1c8a1
Jack Reinhardt <reinh240@umn.edu>
2019-03-12 23:14:39 +0100

    implemented toy search example and execution of moves

--- ai_player.py
+++ ai_player.py
@@ -1,11 +1,18 @@
-import player.py
+from player import Player
 
-class AIPlayer:
-    def __init__(self, thing):
-        self.thing_ = thing
-        Player.__init__(self, None, None)
+class AIPlayer(Player):
+  def __init__(self, name, score):
+      Player.__init__(self, name, score)
 
-    def execute_moves():
-        print("Executing AI moves")
+  # moves is an array of (robot, direction) pairs
+  #   robot: an instance of the Robot class
+  #   direction: a string, "NORTH" "SOUTH" "EAST" or "WEST"
+  # vel is a temporary parameter, we will find a way to avoid passing it
+  # board is also a temporary parameter, we will include it as a member variable
+  def execute_moves(self, board, moves, vel):
+    for i in moves:
+      i[0].move(board, i[1], vel)
+        
 
-    def search_moves():
+  def search(self, board, robots):
+    return [(robots[0], "NORTH")]
