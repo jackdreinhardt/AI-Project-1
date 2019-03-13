0144a17c56adfbf00fde1030d2d85e478d173300
Jack Reinhardt <reinh240@umn.edu>
2019-03-13 09:05:54 +0000

    adjusted tabs

--- square.py
+++ square.py
@@ -1,9 +1,8 @@
 class Square(object): 
-
-    def __init__(self, North, East, South, West, Occupied, Target): 
-        self.north = North 
-        self.east = East 
-        self.south = South 
-        self.west = West
-        self.occ = Occupied
-        self.tar = Target
+  def __init__(self, North, East, South, West, Occupied, Target): 
+    self.north = North 
+    self.east = East 
+    self.south = South 
+    self.west = West
+    self.occ = Occupied
+    self.tar = Target
