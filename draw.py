22afe7a01f2534e9458d23185773a89cbe1f012f
Jack Reinhardt <reinh240@umn.edu>
2019-03-13 10:58:18 +0000

    changes to integrade with app

--- draw.py
+++ draw.py
@@ -10,14 +10,12 @@ import pygame
 
 class GraphicalBoard:
     
-    def __init__(self, boardSize, squareSize, edgeSize, wallthickness, halfthickness, windowTile):
+    def __init__(self, boardSize, squareSize, edgeSize, wallthickness, halfthickness):
         self.BoardSize = boardSize
         self.SquareSize = squareSize
         self.EdgeSize = edgeSize
         self.WallThickness = wallthickness
         self.HalfThickness = halfthickness
-        self.WindowTile= windowTile
-        self.MoveCount = 0
         
         self.WindowSize = (squareSize*boardSize + edgeSize*2, squareSize*boardSize + edgeSize*2)
         self.WindowTitle = "Ricochet Robots"
@@ -40,21 +38,21 @@ class GraphicalBoard:
         
         
         
-    def drawBoardState(self,board,robots):
+    def drawBoardState(self, board, robots):
         pygame.init()
         pygame.font.init()
         pygame.mouse.set_visible(1)
-        self.drawEdges
-        self.drawBoard
-        self.drawRobots
-        self.drawTarget
-        self.drawWalls
+        self.drawEdges()
+        self.drawBoard()
+        self.drawWalls()
+        self.drawRobots(board, robots)
+        self.drawTarget(board)
         
         
     def drawBoard(self):
         for j in range(0, 15, 2):
             for i in range(0, 15, 2):
-                pygame.draw.rect(self.Screen, self.Tile1, (j*self.SquareSize + self.EdgeSize, i*self.SquareSizes + (self.SquareSize + self.EdgeSize), self.SquareSize, self.SquareSize), 0)
+                pygame.draw.rect(self.Screen, self.Tile1, (j*self.SquareSize + self.EdgeSize, i*self.SquareSize + (self.SquareSize + self.EdgeSize), self.SquareSize, self.SquareSize), 0)
         for j in range(0, 15, 2):
             for i in range(0, 15, 2):
                 pygame.draw.rect(self.Screen, self.Tile1, (j*self.SquareSize + self.EdgeSize + self.SquareSize, i*self.SquareSize + self.EdgeSize, self.SquareSize, self.SquareSize), 0)
@@ -67,7 +65,6 @@ class GraphicalBoard:
        pygame.draw.rect(self.Screen, self.Edgecol, (0, 0, self.EdgeSize, self.WindowSize[0]), 0)
        pygame.draw.rect(self.Screen, self.Edgecol, (0, self.WindowSize[0] - self.EdgeSize, self.WindowSize[0], self.EdgeSize), 0)
        pygame.draw.rect(self.Screen, self.Edgecol, (self.WindowSize[0] - self.EdgeSize, 0, self.EdgeSize, self.WindowSize[0]), 0)
-       print("Eecuted DrawEdges")
        
     def drawWalls(self):
         # Draw outer walls
@@ -128,14 +125,14 @@ class GraphicalBoard:
        pygame.draw.rect(self.Screen, self.Black, (self.EdgeSize+14*self.SquareSize-self.HalfThickness, self.EdgeSize+14*self.SquareSize-self.HalfThickness, self.SquareSize+2*self.HalfThickness, self.WallThickness), 0)
        pygame.draw.rect(self.Screen, self.Black, (self.EdgeSize+2*self.SquareSize-self.HalfThickness, self.EdgeSize+15*self.SquareSize-self.HalfThickness, self.SquareSize+2*self.HalfThickness, self.WallThickness), 0)
 
-    def drawRobots(self, robots):
-       #ResetBoard()
-       for i in range(robots.length):
-           pygame.draw.rect(self.Screen, robots[i].Colour, (robots[i].curY,30,30),0)
+    def drawRobots(self, board, robots):
+       self.resetBoard(board)
+       for i in range(len(robots)):
+           pygame.draw.rect(self.Screen, robots[i].colour, (robots[i].curX, robots[i].curY,30,30),0)
        
        pygame.display.update()
 
-    def drawTarget(self,board):
+    def drawTarget(self, board):
        for i in range(self.BoardSize):
           for j in range (self.BoardSize):
               if board[i][j].tar != 0:
@@ -148,10 +145,10 @@ class GraphicalBoard:
                         if board[i][j].tar == 4:
                             pygame.draw.circle(self.Screen, self.Yellow, (self.EdgeSize+j*self.SquareSize+25, self.EdgeSize+i*self.SquareSize+25), 10, 0)
     
-    def resetBoard(self):
+    def resetBoard(self, board):
         self.drawEdges()
         self.drawBoard()
         self.drawWalls()
-        self.drawTarget()
+        self.drawTarget(board)
         pygame.display.update()
 #
