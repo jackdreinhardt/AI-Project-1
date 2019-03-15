22754771f16a1ce3d4856cc9dc8f0af084203b74
Jack Reinhardt <reinh240@umn.edu>
2019-03-15 16:58:43 +0000

    refined move function

--- robot.py
+++ robot.py
@@ -4,8 +4,9 @@
 # moved. 
 
 class Robot: 
-  def __init__(self, Colour, current_square_x, current_square_y): 
-    self.colour = Colour 
+  def __init__(self, Colour, current_square_x, current_square_y, id):
+    self.id_ = id
+    self.colour = Colour
     self.curSx = current_square_x
     self.curSy = current_square_y
 
@@ -14,31 +15,27 @@ class Robot:
   # @param d direction to move robot
   # @param l number of pixels to move ('vel' in RicochetRobots.py)
   # returns true if move was successful, false otherwise
-  def move(self, board, d, l):
+  def move(self, board, d):
     # test_robot = self
     moved = False
     if d == "NORTH":
       while board[self.curSy][self.curSx].north == 0 and board[self.curSy-1][self.curSx].occ == 0:
         self.curSy -= 1
-        
         moved = True
         # update board[][].robot
     elif d == "SOUTH":
       while board[self.curSy][self.curSx].south == 0 and board[self.curSy+1][self.curSx].occ == 0:
         self.curSy += 1
-        
         moved = True
         # update board[][].robot
     elif d == "EAST":
       while board[self.curSy][self.curSx].east == 0 and board[self.curSy][self.curSx+1].occ == 0:
         self.curSx += 1
-        
         moved = True
         # update board[][].robot
     elif d == "WEST":
       while board[self.curSy][self.curSx].west == 0 and board[self.curSy][self.curSx-1].occ == 0:
         self.curSx -= 1
-        
         moved = True
         # update board[][].robot
     else:
