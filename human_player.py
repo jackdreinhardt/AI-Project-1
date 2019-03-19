93ead15e0237e7b91627674a4d7ebde24e5c983d
Jack Reinhardt <reinh240@umn.edu>
2019-03-19 16:56:32 +0100

    convert tabs to 4 spaces

--- human_player.py
+++ human_player.py
@@ -3,13 +3,13 @@ from RicochetRobots import KeyToDir, board, vel
 
 # not implemented correctly
 class HumanPlayer(Player):
-  def execute_moves(self, board, moves, vel):
-    for event in pygame.event.get():                    
-      if event.type == pygame.MOUSEBUTTONDOWN:
-        click = event.pos
-        currentRobo = DetermineRobo(click)
-      if event.type == pygame.KEYDOWN and currentRobo != 0:
-        currentRobo.move(board, KeyToDir(event.key))
+    def execute_moves(self, board, moves, vel):
+        for event in pygame.event.get():                    
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = event.pos
+                currentRobo = DetermineRobo(click)
+            if event.type == pygame.KEYDOWN and currentRobo != 0:
+                currentRobo.move(board, KeyToDir(event.key))
 
   # since the user is playing the game, no search algorithm is needed
   def search(self, board, robots, limit):
