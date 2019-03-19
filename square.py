c4c441e1c12a485b92dd7b5f72f501dfd76b001e
Jack Reinhardt <reinh240@umn.edu>
2019-03-19 17:02:20 +0100

    changed tab width

--- square.py
+++ square.py
@@ -1,18 +1,18 @@
 class Square(object): 
-  def __init__(self): 
-    self.north_ = False 
-    self.east_ = False 
-    self.south_ = False
-    self.west_ = False
-    self.target_ = None
-    self.robot_ = None
+    def __init__(self): 
+        self.north_ = False 
+        self.east_ = False 
+        self.south_ = False
+        self.west_ = False
+        self.target_ = None
+        self.robot_ = None
 
-  def Wall(self, direction):
-    if direction == NORTH:
-      self.north_ = True
-    elif direction == SOUTH:
-      self.south_ = True
-    elif direction == EAST:
-      self.east_ = True
-    elif direction == WEST:
-      self.south_ = True
+    def Wall(self, direction):
+        if direction == NORTH:
+            self.north_ = True
+        elif direction == SOUTH:
+            self.south_ = True
+        elif direction == EAST:
+            self.east_ = True
+        elif direction == WEST:
+            self.south_ = True
