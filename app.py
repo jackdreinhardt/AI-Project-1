19ccd795c329e475ba49407de8305ef79793b33e
Jack Reinhardt <reinh240@umn.edu>
2019-03-19 17:01:55 +0100

    changed tab width

--- app.py
+++ app.py
@@ -12,86 +12,91 @@ from BF_Graph_Search import Graph_Search_BF
 
 
 class App:
-  def __init__(self):
-    self.robots_ = (Robot(RED, 13, 12), Robot(BLUE, 5, 11),
-                    Robot(GREEN, 3, 6), Robot(YELLOW, 13, 0))
-    self.state_ = Board(self.robots_)
-    self.graphics_ = GraphicalBoard(BOARDSIZE)
+    def __init__(self):
+        self.robots_ = [Robot(RED, 13, 12), Robot(BLUE, 5, 11),
+                        Robot(GREEN, 3, 6), Robot(YELLOW, 13, 0)]
+        self.state_ = Board(self.robots_)
+        self.graphics_ = GraphicalBoard(BOARDSIZE)
 
-  def KeyToDir(self, key):
-    if key == pygame.K_UP:
-        return "NORTH"
-    elif key == pygame.K_DOWN:
-        return "SOUTH"
-    elif key == pygame.K_RIGHT:
-        return "EAST"
-    elif key == pygame.K_LEFT:
-        return "WEST"
-    else:
-        return "NOT SUPPORTED"
+    def KeyToDir(self, key):
+        if key == pygame.K_UP:
+            return "NORTH"
+        elif key == pygame.K_DOWN:
+            return "SOUTH"
+        elif key == pygame.K_RIGHT:
+            return "EAST"
+        elif key == pygame.K_LEFT:
+            return "WEST"
+        else:
+            return "NOT SUPPORTED"
 
-  def RunAI(self):
-    self.state_.PlaceWalls()
-    self.state_.PlaceRobots()
-    self.state_.PlaceTarget()
+    def RunAI(self):
+        self.state_.PlaceWalls()
+        self.state_.PlaceRobots()
+        self.state_.PlaceTarget()
 
-    self.graphics_.drawBoardState(self.state_.board_, self.robots_)
+        self.graphics_.drawBoardState(self.state_.board_, self.robots_)
 
-    robot = None
-    moveCount = 0
+        robot = None
+        moveCount = 0
 
-    while True:
-      pygame.time.delay(100)
-      ai_player = Depth_Limited_Player()
-      print("Searching for solution")
-      solution = ai_player.search(self.state_.board_, self.robots_, 3)
-      if (solution != "FAILURE" and solution != "CUTOFF"):
-        print("Found solution")
-        for m in range(len(solution)):
-          #print(solution)
-          self.robots_[solution[m][0]].move(self.state_.board_, solution[m][1]) # move robot
-          self.graphics_.drawRobots(self.state_.board_, self.robots_)
-          time.sleep(1)
-          break
-      else:
-        print("No solution found")
-        break
+        while True:
+            pygame.time.delay(100)
+            ai_player = Depth_Limited_Player()
+            print("Searching for solution")
+            solution = ai_player.search(self.state_.board_, self.robots_, 3)
+            if (solution != "FAILURE" and solution != "CUTOFF"):
+                print("Found solution")
+                for m in range(len(solution)):
+                    #print(solution)
+                    self.robots_[solution[m][0]].move(self.state_.board_, solution[m][1]) # move robot
+                    self.graphics_.drawRobots(self.state_.board_, self.robots_)
+                    time.sleep(1)
+            else:
+                print("No solution found")
+            self.state_.PlaceTarget()
+            self.graphics_.drawRobots(self.state_.board_, self.robots_)
+            pygame.display.update()
+        
+      
 
-  def Run(self):
-    self.state_.PlaceWalls()
-    self.state_.PlaceRobots()
-    self.state_.PlaceTarget()
+    def Run(self):
+        self.state_.PlaceWalls()
+        self.state_.PlaceRobots()
+        self.state_.PlaceTarget()
 
-    self.graphics_.drawBoardState(self.state_.board_, self.robots_)
+        self.graphics_.drawBoardState(self.state_.board_, self.robots_)
 
-    robot = None
-    moveCount = 0
+        robot = None
+        moveCount = 0
 
-    while True:
-      pygame.time.delay(100)
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          pygame.display.quit()
-          pygame.quit()
-          exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-          robot = self.graphics_.DetermineRobo(event.pos, self.robots_)
-        if event.type == pygame.KEYDOWN and robot != None:
-          d = self.KeyToDir(event.key)
-          if robot.move(self.state_.board_, d):
-              moveCount += 1
-          self.graphics_.drawRobots(self.state_.board_, self.robots_)
+        while True:
+            pygame.time.delay(100)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.display.quit()
+                    pygame.quit()
+                    exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    robot = self.graphics_.DetermineRobo(event.pos, self.robots_)
+                if event.type == pygame.KEYDOWN and robot != None:
+                    d = self.KeyToDir(event.key)
+                    if robot.move(self.state_.board_, d):
+                        moveCount += 1
+                    self.graphics_.drawRobots(self.state_.board_, self.robots_)
 
-          for r in self.robots_:
-            if self.state_.board_[r.y_][r.x_].target_ != None and self.state_.board_[r.y_][r.x_].target_.color_ == r.color_:
-              print("Success! New target placed")
-              print("You took " + str(moveCount) + " moves to find a solution")
-              moveCount = 0
-              self.state_.PlaceTarget()
-              self.graphics_.drawRobots(self.state_.board_, self.robots_)
-          print("Moves: " + str(moveCount))
-      pygame.display.update()
+                    for r in self.robots_:
+                        if self.state_.board_[r.y_][r.x_].target_ != None and self.state_.board_[r.y_][r.x_].target_.color_ == r.color_:
+                            print("Success! New target placed")
+                            print("You took " + str(moveCount) + " moves to find a solution")
+                            moveCount = 0
+                            self.state_.PlaceTarget()
+                            self.graphics_.drawRobots(self.state_.board_, self.robots_)
+                    print("Moves: " + str(moveCount))
+            pygame.display.update()
 
 if __name__ == '__main__':
   game = App()
-  game.RunAI()
+  game.Run()
+
+
