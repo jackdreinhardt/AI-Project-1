afeae5392304453b14d76a0ad11833087d3f0fac
Jack Reinhardt <reinh240@umn.edu>
2019-03-20 13:09:08 +0100

    changed board.center call

--- target.py
+++ target.py
@@ -3,41 +3,35 @@ from globals import *
 from board import Board
 
 class Target:
-    def __init__(self, boardSize, board, num_robots):
+    def __init__(self, boardSize, board, robots):
         self.board_size_ = boardSize
-        self.set_target(board, num_robots)
+        self.set_target(board, robots)
 
-    def set_target(self, board, num_robots):
-        colors = [RED, BLUE, GREEN, YELLOW]
-        self.color_ = colors[rd.randrange(num_robots)]
-        locations = self.valid_locations(board)
+    def set_target(self, board, robots):
+        self.color_ = robots[rd.randrange(len(robots))].color_
+        locations = self.valid_locations(board, robots)
         loc = locations[rd.randrange(len(locations))]
         self.x_ = loc[1]
         self.y_ = loc[0]
 
-    def valid_target_loc(self, board, i, j):
-        return (board.square(i, j).wall_north_ and board.square(i, j).wall_east_) \
+    def valid_target_loc(self, board, robots, i, j):
+        return ((board.square(i, j).wall_north_ and board.square(i, j).wall_east_) \
             or (board.square(i, j).wall_north_ and board.square(i, j).wall_west_) \
             or (board.square(i, j).wall_south_ and board.square(i, j).wall_east_) \
-            or (board.square(i, j).wall_south_ and board.square(i, j).wall_west_) \
-            and not (i == 0 or i ==  self.board_size_ - 1) \
-            and not (j == 0 or j == self.board_size_ - 1) \
-            and not board.center(board, i, j)
+            or (board.square(i, j).wall_south_ and board.square(i, j).wall_west_)) \
+            and not board.center(i, j) \
+            and not (i,j) in [(r.y_,r.x_) for r in robots]
+            # and not (i == 0 or i ==  self.board_size_ - 1) \
+            # and not (j == 0 or j == self.board_size_ - 1) \
+            
 
-    def valid_locations(self, board):
+    def valid_locations(self, board, robots):
         locations = []
         for i in range(self.board_size_):
             for j in range(self.board_size_):
-                if self.valid_target_loc(board, i, j):
+                if self.valid_target_loc(board, robots, i, j):
                     locations.append((i,j))
         return locations
-
-
-
-t = Target(6, Board(6), 4)
-for i in range(0,6):
-    for j in range(0,6):
-        print(i, j, t.valid_target_loc(Board(6), i, j))
 
 # def PlaceTarget(self):
 #         for i in range(BOARDSIZE):
