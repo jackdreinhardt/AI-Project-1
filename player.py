0eca7f4926571d54a9b0d53ab11151d62e30e7c0
Jack Reinhardt <reinh240@umn.edu>
2019-03-25 22:53:52 +0100

    Added graphical elements because fuck it

--- player.py
+++ player.py
@@ -3,6 +3,13 @@ class Player:
         self.name_ = name
         self.score_ = score
 
+        self.north_click_bound = None
+        self.south_click_bound = None
+        self.east_click_bound = None
+        self.west_click_bound = None
+
+        self.selected_ = False
+
     # pure virtual, implemented by child class
     def execute_moves(self, app, limit):
         raise TypeError('Abstract method `' + self._class.__name__ \
@@ -12,3 +19,4 @@ class Player:
     def search(self, board, target, robots):
         raise TypeError('Abstract method `' + self._class.__name__ \
                             + '.' + self._function + '\' called')
+
