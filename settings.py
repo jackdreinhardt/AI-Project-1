e4589d7969bdddccdb67ba8b1c9ce604728abaef
Jack Reinhardt <reinh240@umn.edu>
2019-03-25 23:17:12 +0100

    changed -t flag to T/F

--- settings.py
+++ settings.py
@@ -16,9 +16,7 @@ class Settings:
                     raise "Cannot have more than 4 robots"
                 self.robots_ = int(args[i+1])
             elif args[i] == '-t':
-                if int(args[i+1]) < 1:
-                    raise "Cannot test less 1 round"
-                self.test_rounds_ = int(args[i+1])
+                self.test_rounds_ = True
             elif args[i] == '-p1':
                 self.players_[0] = args[i+1]
             elif args[i] == '-p2':
