02504729f54eab8cc9ff77d846aaf9b16ba0e0db
Jack Reinhardt <reinh240@umn.edu>
2019-04-01 15:52:31 +0200

    Added comments

--- board.py
+++ board.py
@@ -1,6 +1,10 @@
 import random
-
 from square import Square
+
+# Board class
+#
+# Provides the internal data structure for the board. Initializes the squares of
+# the board in a matrix, and places walls.
 
 class Board:
     def __init__(self, boardSize):
@@ -22,20 +26,17 @@ class Board:
 
     def PlaceWalls(self, boardsize):
         if (boardsize==16):
-            
             # outer walls
             for i in range(boardsize):
                 self.board_[0][i].wall_north_ = 1
                 self.board_[15][i].wall_south_ = 1
                 self.board_[i][0].wall_west_ = 1
                 self.board_[i][15].wall_east_ = 1
-    
             # Add inner square walls
             self.board_[6][7].wall_south_ = self.board_[6][8].wall_south_ = 1
             self.board_[7][6].wall_east_ = self.board_[8][6].wall_east_ = 1
             self.board_[7][9].wall_west_ = self.board_[8][9].wall_west_ = 1
             self.board_[9][7].wall_north_ = self.board_[9][8].wall_north_ = 1
-    
             # Add vertical walls
             self.board_[0][3].wall_east_ = self.board_[0][4].wall_west_ = 1
             self.board_[0][10].wall_east_ = self.board_[0][11].wall_west_ = 1
@@ -58,7 +59,6 @@ class Board:
             self.board_[14][14].wall_east_ = self.board_[14][15].wall_west_ = 1
             self.board_[15][5].wall_east_ = self.board_[15][6].wall_west_ = 1
             self.board_[15][11].wall_east_ = self.board_[15][12].wall_west_ = 1
-
             # Add horizontal walls
             self.board_[0][5].wall_south_ = self.board_[1][5].wall_north_ = 1
             self.board_[0][12].wall_south_ = self.board_[1][12].wall_north_ = 1
@@ -81,28 +81,23 @@ class Board:
             self.board_[13][9].wall_south_ = self.board_[14][9].wall_north_ = 1
             self.board_[13][14].wall_south_ = self.board_[14][14].wall_north_ = 1
             self.board_[14][2].wall_south_ = self.board_[15][2].wall_north_ = 1
-        
         if (boardsize==6):
-        
-        # outer walls
+            # outer walls
             for i in range(boardsize):
                 self.board_[0][i].wall_north_ = 1
                 self.board_[boardsize-1][i].wall_south_ = 1
                 self.board_[i][0].wall_west_ = 1
                 self.board_[i][boardsize-1].wall_east_ = 1
-
             # Add inner square walls
             self.board_[1][2].wall_south_ = self.board_[1][3].wall_south_ = 1
             self.board_[3][2].wall_south_ = self.board_[3][3].wall_south_ = 1
             self.board_[2][2].wall_north_ = self.board_[2][3].wall_north_ = 1
             self.board_[4][2].wall_north_ = self.board_[4][3].wall_north_ = 1
             
-            
             self.board_[2][1].wall_east_=self.board_[2][2].wall_west_=1
             self.board_[3][1].wall_east_=self.board_[3][2].wall_west_=1
             self.board_[2][3].wall_east_=self.board_[2][4].wall_west_=1
             self.board_[3][3].wall_east_=self.board_[3][4].wall_west_=1
-            
                 
             self.board_[1][0].wall_south_= self.board_[2][0].wall_north_=1
             self.board_[3][0].wall_south_=self.board_[4][0].wall_north_=1
@@ -113,10 +108,4 @@ class Board:
             self.board_[1][1].wall_east_=self.board_[1][2].wall_west_=1
             self.board_[0][4].wall_east_=self.board_[0][5].wall_west_=1
             self.board_[3][4].wall_east_=self.board_[3][5].wall_west_=1
-            self.board_[5][2].wall_east_=self.board_[5][3].wall_west_=1
-        
-        
-        
-                
-            
-
+            self.board_[5][2].wall_east_=self.board_[5][3].wall_west_=1  
