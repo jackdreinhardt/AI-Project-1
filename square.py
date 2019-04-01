7ba4373159fef040856d80c8c0c35dcd98fbf276
Jack Reinhardt <reinh240@umn.edu>
2019-04-01 14:33:13 +0200

    added comments and deleted number_walls function

--- square.py
+++ square.py
@@ -1,17 +1,11 @@
+# Square class
+#
+# Simple object to represent each space in the board. Includes the variables
+# needed to represent the walls on each board space
+
 class Square(object): 
     def __init__(self): 
         self.wall_north_ = False 
         self.wall_east_ = False 
         self.wall_south_ = False
         self.wall_west_ = False
-        
-        
-    def number_walls(self):
-        
-        n =0
-        if (self.wall_north_): n=n+1
-        if self.wall_east_: n+=1
-        if self.wall_south_: n+=1
-        if self.wall_west_: n+=1
-        
-        return n
