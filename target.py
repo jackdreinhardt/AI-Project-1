338eb2cf92745e7d57d0e1d38a261409918d740b
Jack Reinhardt <reinh240@umn.edu>
2019-04-01 14:51:21 +0200

    added comments and fixed valid target loc function

--- target.py
+++ target.py
@@ -1,9 +1,10 @@
-import random as rd
-from globals import *
-from board import Board
-from robot import Robot
-from square import Square
-import copy
+from random import randrange as rd
+
+# Target class
+#
+# Provides functions to randomly place the target in a valid location.
+# Additionally, contains x and y coordinates, and color of the target
+
 
 class Target:
     def __init__(self, boardSize, board, robots):
@@ -11,11 +12,9 @@ class Target:
         self.set_target(board, robots)
 
     def set_target(self, board, robots):
-        self.color_ = robots[rd.randrange(len(robots))].color_
+        self.color_ = robots[rd(len(robots))].color_
         locations = self.valid_locations(board, robots)
-        loc = locations[rd.randrange(len(locations))]
-        self.x_ = loc[1]
-        self.y_ = loc[0]
+        self.y_, self.x_ = locations[rd(len(locations))]
 
     def valid_target_loc(self, board, robots, i, j):
         return ((board.square(i, j).wall_north_ and board.square(i, j).wall_east_) \
@@ -23,10 +22,9 @@ class Target:
             or (board.square(i, j).wall_south_ and board.square(i, j).wall_east_) \
             or (board.square(i, j).wall_south_ and board.square(i, j).wall_west_)) \
             and not board.center(i, j) \
-            and not (i,j) in [(r.y_,r.x_) for r in robots]
-            # and not (i == 0 or i ==  self.board_size_ - 1) \
-            # and not (j == 0 or j == self.board_size_ - 1) \
-            
+            and not (i,j) in [(r.y_,r.x_) for r in robots] \
+            and (self.board_size_ == 6 or not (i == 0 or i ==  self.board_size_ - 1)) \
+            and (self.board_size_ == 6 or not (j == 0 or j == self.board_size_ - 1))     
 
     def valid_locations(self, board, robots):
         locations = []
@@ -35,5 +33,4 @@ class Target:
                 if self.valid_target_loc(board, robots, i, j):
                     locations.append((i,j))
         return locations
-    
     
